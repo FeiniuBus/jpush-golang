c5956e4ae0c7461f79aae095e77bb7ba6a30094e
Xu Qiaolun <Jamesxql@Gmail.com>
2016-11-16 05:27:12 +0800

    commit platform and audience model

--- jpush/andience.go
+++ jpush/andience.go
@@ -1,4 +1,142 @@
 package jpush
 
+// Audience is
 type Audience struct {
+	Audiences map[string][]string `json:"dictionary,omitempty"`
 }
+
+// SetTag is
+func (audience *Audience) SetTag(value ...string) *Audience {
+	target := new(AudienceTarget)
+	target.SetTag(value)
+	audience.addWithAudienceTarget(target)
+	return audience
+}
+
+// SetTagAnd is
+func (audience *Audience) SetTagAnd(value ...string) *Audience {
+	target := new(AudienceTarget)
+	target.SetTagAnd(value)
+	audience.addWithAudienceTarget(target)
+	return audience
+}
+
+// SetAlias is
+func (audience *Audience) SetAlias(value ...string) *Audience {
+	target := new(AudienceTarget)
+	target.SetAlias(value)
+	audience.addWithAudienceTarget(target)
+	return audience
+}
+
+// SetSegment is
+func (audience *Audience) SetSegment(value ...string) *Audience {
+	target := new(AudienceTarget)
+	target.SetSegment(value)
+	audience.addWithAudienceTarget(target)
+	return audience
+}
+
+// SetRegistrationID is
+func (audience *Audience) SetRegistrationID(value ...string) *Audience {
+	target := new(AudienceTarget)
+	target.SetRegistrationID(value)
+	audience.addWithAudienceTarget(target)
+	return audience
+}
+
+func (audience *Audience) addWithAudienceTarget(target *AudienceTarget) *Audience {
+	if target.ValueBuilder != nil {
+		if audience.Audiences == nil {
+			audience.Audiences = make(map[string][]string)
+		}
+
+		if val, ok := audience.Audiences[target.Type]; ok {
+			val = append(val, target.ValueBuilder...)
+			audience.Audiences[target.Type] = val
+		} else {
+			audience.Audiences[target.Type] = target.ValueBuilder
+		}
+	}
+	return audience
+}
+
+// AudienceTarget is
+type AudienceTarget struct {
+	Type         string   `json:"andienceType,omitempty"`
+	ValueBuilder []string `json:"valueBuilder,omitempty"`
+}
+
+// SetTag is
+func (at *AudienceTarget) SetTag(values []string) *AudienceTarget {
+	at.Type = Tag.String()
+	at.ValueBuilder = values
+
+	return at
+}
+
+// SetTagAnd is
+func (at *AudienceTarget) SetTagAnd(values []string) *AudienceTarget {
+	at.Type = TagAnd.String()
+	at.ValueBuilder = values
+
+	return at
+}
+
+// SetAlias is
+func (at *AudienceTarget) SetAlias(values []string) *AudienceTarget {
+	at.Type = Alias.String()
+	at.ValueBuilder = values
+
+	return at
+}
+
+// SetSegment is
+func (at *AudienceTarget) SetSegment(values []string) *AudienceTarget {
+	at.Type = Segment.String()
+	at.ValueBuilder = values
+
+	return at
+}
+
+// SetRegistrationID is
+func (at *AudienceTarget) SetRegistrationID(values []string) *AudienceTarget {
+	at.Type = RegistrationID.String()
+	at.ValueBuilder = values
+
+	return at
+}
+
+// AudienceType is
+type AudienceType int
+
+// String is
+func (at AudienceType) String() string {
+	switch at {
+	case Tag:
+		return "tag"
+	case TagAnd:
+		return "tag_and"
+	case Alias:
+		return "alias"
+	case Segment:
+		return "segment"
+	case RegistrationID:
+		return "registration_id"
+	}
+
+	return ""
+}
+
+const (
+	// Tag is
+	Tag AudienceType = iota
+	// TagAnd is
+	TagAnd
+	// Alias is
+	Alias
+	// Segment is
+	Segment
+	// RegistrationID is
+	RegistrationID
+)
--- jpush/platform.go
+++ jpush/platform.go
@@ -1,4 +1,61 @@
 package jpush
 
+// Platform is
 type Platform struct {
+	DeviceTypes []string `json:"deviceTypes,omitempty"`
 }
+
+// Ios is
+func (plat *Platform) Ios() *Platform {
+	if plat.DeviceTypes == nil {
+		plat.DeviceTypes = make([]string, 0)
+	}
+
+	plat.DeviceTypes = append(plat.DeviceTypes, DeviceType.String(Ios))
+	return plat
+}
+
+// Android is
+func (plat *Platform) Android() *Platform {
+	if plat.DeviceTypes == nil {
+		plat.DeviceTypes = make([]string, 0)
+	}
+
+	plat.DeviceTypes = append(plat.DeviceTypes, DeviceType.String(Android))
+	return plat
+}
+
+// Winphone is
+func (plat *Platform) Winphone() *Platform {
+	if plat.DeviceTypes == nil {
+		plat.DeviceTypes = make([]string, 0)
+	}
+
+	plat.DeviceTypes = append(plat.DeviceTypes, DeviceType.String(Winphone))
+	return plat
+}
+
+// DeviceType is
+type DeviceType int
+
+func (dt DeviceType) String() string {
+	switch dt {
+	case Android:
+		return "android"
+	case Ios:
+		return "ios"
+	case Winphone:
+		return "winphone"
+	}
+
+	return ""
+}
+
+const (
+	// Android is
+	Android DeviceType = iota
+	// Ios is
+	Ios
+	// Winphone is
+	Winphone
+)
